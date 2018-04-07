340085f4553ad8de2afcafc56eb5a8ea5e8fe88f
BrianArchibald <brianallenarchibald@gmail.com>
2018-04-07 12:21:32 -0500

    new system for timer

--- countdown.js
+++ countdown.js
@@ -1,47 +1,47 @@
 
 //not sure if (eventDate) is correct below
-function getTimeRemaining (endtime)  {
+// function getTimeRemaining (endtime)  {
 
-	//var event = document.getElementById('event');
+// 	//var event = document.getElementById('event');
 	
-	//var eventHour = document.getElementById('eventHour');
-	//var eventMinute = document.getElementById('eventMinute'); 
-	//var eventSeconds = document.getElementById('eventSeconds');
-	//var eventAmPm = document.getElementById('eventAmPm');
+// 	//var eventHour = document.getElementById('eventHour');
+// 	//var eventMinute = document.getElementById('eventMinute'); 
+// 	//var eventSeconds = document.getElementById('eventSeconds');
+// 	//var eventAmPm = document.getElementById('eventAmPm');
 
-	var remainingTime = Date.parse(endtime) - Date.parse(new Date());
-	var seconds = Math.floor( (remainingTime/1000) % 60);
-	var minutes = Math.floor( (remainingTime/1000/60) % 60);
-	var hours = Math.floor( (remainingTime/(1000*60*60)) % 24);
-	var days = Math.floor( remainingTime/(1000*60*60*24));
+// 	var remainingTime = Date.parse(endtime) - Date.parse(new Date());
+// 	var seconds = Math.floor( (remainingTime/1000) % 60);
+// 	var minutes = Math.floor( (remainingTime/1000/60) % 60);
+// 	var hours = Math.floor( (remainingTime/(1000*60*60)) % 24);
+// 	var days = Math.floor( remainingTime/(1000*60*60*24));
 
-	return {
-		'remainingTime' : remainingTime,
-		'days' : days,
-		'hours' : hours,
-		'minutes' : minutes,
-		'seconds' : seconds
-	};
-}
+// 	return {
+// 		'remainingTime' : remainingTime,
+// 		'days' : days,
+// 		'hours' : hours,
+// 		'minutes' : minutes,
+// 		'seconds' : seconds
+// 	};
+// }
 
-function startTimer (id, endtime) {
-	var clock = document.getElementById(id);
-	function updateClock(){
-	var remainingTime = getTimeRemaining(endtime);
-		clock.innerHTML =  'days: ' + remainingTime.days + '<br>' +
-						   'hours: ' + remainingTime.hours + "<br>" +
-						   'minutes: ' + remainingTime.minutes + '<br>' +
-						   'seconds: ' + remainingTime.seconds;
+// function startTimer (id, endtime) {
+// 	var clock = document.getElementById(id);
+// 	function updateClock(){
+// 	var remainingTime = getTimeRemaining(endtime);
+// 		clock.innerHTML =  'days: ' + remainingTime.days + '<br>' +
+// 						   'hours: ' + remainingTime.hours + "<br>" +
+// 						   'minutes: ' + remainingTime.minutes + '<br>' +
+// 						   'seconds: ' + remainingTime.seconds;
 
-		if(remainingTime.total <=0) {
-			clearInterval(timeInterval);
-		}
-	}
+// 		if(remainingTime.total <=0) {
+// 			clearInterval(timeInterval);
+// 		}
+// 	}
 
-	updateClock(); // run function once at first without delay
-	var timeInterval = setInterval(updateClock, 1000);
+// 	updateClock(); // run function once at first without delay
+// 	var timeInterval = setInterval(updateClock, 1000);
 
-var eventDate = document.getElementById('eventDate');
+// var eventDate = document.getElementById('eventDate');
 
 
 
@@ -59,3 +59,28 @@ var eventDate = document.getElementById('eventDate');
 // update every second
 
 //message when timer is completed
+
+
+var eventDate = new Date(/* get event date through input */).getTime();
+
+var interval = setInterval(function() {
+
+	var now = new Date().getTime();
+
+	var remainingTime = eventDate - now;
+
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById('timer').innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+    	clearInterval(interval);
+    	document.getElementById('timer').innerHTML = "Times Up!";
+    }
+
+
+}, 1000);
